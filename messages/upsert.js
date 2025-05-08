dae749247036a84c9c9b3b6baad5b0f4fa08aec6
inkyssj <damus203@gmail.com>
2025-05-08 12:57:50 -0300

    Update upsert.js

--- messages/upsert.js
+++ messages/upsert.js
@@ -39,7 +39,7 @@ export async function upsert(sock, m, plugins) {
 				let text = m.body.slice(2)
 				let trimmedText = text.trim()
 				if (!trimmedText) return
-				let evaled = await eval((async() => { `${trimmedText}` })())
+				let evaled = await eval((async() => { trimmedText })())
 				if (typeof evaled !== 'string') {
 					evaled = util.inspect(evaled)
 				}
